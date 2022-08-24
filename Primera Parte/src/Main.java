a34f1a70900fa9c209a60d41443ad23e93e5645b
AniGalleta <111407611+AniGalleta@users.noreply.github.com>
2022-08-23 21:00:15 -0500

    Add files via upload

---
+++ Primera Parte/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        suma( 2 , 4, 5);
+    }
+
+    public static void suma(int a, int b, int c) {
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(a - b);
+        System.out.println(a + c);
+        System.out.println(a * c);
+    }
+}
